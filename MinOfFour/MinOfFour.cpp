6d72ff697c5ae1c93420dc3819419342b9afe6b5
文汰 村田 <kmm2459394@stu.o-hara.ac.jp>
2024-06-18 14:05:59 +0900

    課題３

---
+++ MinOfFour/MinOfFour.cpp
@@ -0,0 +1,32 @@
+//=====================================
+// 4値の最小値
+//=====================================
+#include <stdio.h>
+#include <stdlib.h>  // srand(),rand()
+#include <time.h>    // time()
+// 関数プロトタイプ
+int MinOfFour(int a, int b, int c, int d);
+
+int main()
+{
+	srand(time(nullptr));
+	int a = rand() % 100;
+	int b = rand() % 100;
+	int c = rand() % 100;
+	int d = rand() % 100;
+	int ans = MinOfFour(a, b, c, d);
+	printf("MinOfFour(%d,%d,%d,%d) => %d\n", a, b, c, d, ans);
+	return 0;
+}
+int MinOfFour(int a, int b, int c, int d)
+{
+	int min = a;
+	if (b > min)
+		b = min;
+	if (c > min)
+		c = min;
+	if (d > min)
+		d = min;
+
+	return min;
+}
