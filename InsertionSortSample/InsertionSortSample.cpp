4d3125693d7baafba0b444281ccf6a5dc11d621b
文汰 村田 <kmm2459394@stu.o-hara.ac.jp>
2024-07-01 10:23:17 +0900

    insertion

---
+++ InsertionSortSample/InsertionSortSample.cpp
@@ -0,0 +1,66 @@
+//=========================================================
+//	単純挿入ソート
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>   // printf(),scanf()
+#include <stdlib.h>  // srand(),rand()
+#include <time.h>    // time()
+// 関数プロトタイプ
+void InsertionSort(int array[], int arraySize);
+int getRandRange(int min, int max);
+void dumpArray(const int array[], int arraySize);
+
+int main()
+{
+	srand(time(nullptr));
+	int arraySize;
+	int* array;
+
+	while (true) {
+		do {
+			printf("要素数:");
+			scanf("%d", &arraySize);
+		} while (arraySize <= 2);
+
+		array = (int*)calloc(arraySize, sizeof(int));
+		if (array == nullptr) {
+			printf("calloc()失敗\n");
+			exit(1);
+		}
+		for (int i = 0; i < arraySize; i++) {
+			array[i] = getRandRange(1, 100);
+		}
+		dumpArray(array, arraySize);
+		InsertionSort(array, arraySize);
+		printf("昇順にソートしました\n");
+		dumpArray(array, arraySize);
+
+		free(array);
+	}
+	return 0;
+}
+
+void InsertionSort(int array[], int arraySize)
+{
+	for (int i = 1; i < arraySize; i++) {
+		int tmp = array[i];
+		int j;
+		for (j = i; j > 0 && array[j - 1] > tmp; j--) {
+			array[j] = array[j - 1];
+		}
+		array[j] = tmp;
+	}
+}
+
+void dumpArray(const int array[], int arraySize)
+{
+	for (int i = 0; i < arraySize; i++) {
+		printf("array[%d] = %d\n", i, array[i]);
+	}
+}
+
+int getRandRange(int min, int max)
+{
+	int wid = max - min + 1;
+	return min + rand() % wid;
+}
