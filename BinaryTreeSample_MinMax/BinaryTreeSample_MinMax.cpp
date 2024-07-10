9a68fd9aeb4d9ed8af733dd48078f99c5c548f52
文汰 村田 <kmm2459394@stu.o-hara.ac.jp>
2024-07-10 12:40:33 +0900

    演習9-2

---
+++ BinaryTreeSample_MinMax/BinaryTreeSample_MinMax.cpp
@@ -0,0 +1,96 @@
+//==========================================================
+//	２分探索木サンプル MinMax
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include "member.h"
+#include "BinaryTree.h"
+#include <stdio.h> // printf(),scanf()
+
+typedef enum {
+	TERMINATE,
+	ADD,
+	REMOVE,
+	SEARCH,
+	PRINT,
+	GET_MIN,
+	GET_MAX,
+} Menu;
+
+// 関数プロトタイプ
+Menu SelectMenu();
+void setup(BinNode** root);
+
+int main()
+{
+	Menu menu;
+	BinNode* root = nullptr;
+
+	setup(&root);
+	do {
+		Member x;
+		BinNode* temp;
+		menu = SelectMenu();
+		switch (menu) {
+		case ADD:
+			x = ScanMember("挿入", MEMBER_NO + MEMBER_NAME);
+			root = Add(root, &x);
+			break;
+		case REMOVE:
+			x = ScanMember("削除", MEMBER_NO);
+			Remove(&root, &x);
+			break;
+		case SEARCH:
+			x = ScanMember("探索", MEMBER_NO);
+			temp = Search(root, &x);
+			if (temp != nullptr) {
+				PrintLnMember(&temp->data);
+			}
+			break;
+		case PRINT:
+			puts("【一覧表】");
+			PrintTree(root);
+			break;
+		case GET_MIN:
+			temp = GetMinNode(root);
+			if (temp != nullptr) {
+				puts("最小値は");
+				PrintLnMember(&temp->data);
+			}
+			break;
+		case GET_MAX:
+			temp = GetMaxNode(root);
+			if (temp != nullptr) {
+				puts("最大値は");
+				PrintLnMember(&temp->data);
+			}
+			break;
+		}
+	} while (menu != TERMINATE);
+
+	FreeTree(root);
+	return 0;
+}
+
+Menu SelectMenu()
+{
+	int ch;
+	do {
+		printf("\n(1)挿入 (2)削除 (3)探索 (4)表示 (5)最小値 (6)最大値 (0)終了:");
+		scanf("%d", &ch);
+	} while (ch < TERMINATE || GET_MAX < ch);
+	return (Menu)ch;
+}
+void setup(BinNode** root)
+{
+	Member data[] = {
+		{1,"赤尾"},
+		{10,"小野"},
+		{5,"武田"},
+		{12,"鈴木"},
+		{14,"神崎"},
+	};
+	int dataSize = sizeof(data) / sizeof(data[0]);
+	for (int i = 0; i < dataSize; i++) {
+		*root = Add(*root, &data[i]);
+	}
+}
